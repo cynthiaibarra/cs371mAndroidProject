51e5b84e05a1d3ceef4f3b49ab614d681a0773bd
Cynthia Ibarra <cibarra@utexas.edu>
2016-10-28 10:30:13 -0500

    starting layout froms scratch

---
+++ Project/app/src/main/java/cai288/cs371m/project/MainActivity.java
@@ -0,0 +1,13 @@
+package cai288.cs371m.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
